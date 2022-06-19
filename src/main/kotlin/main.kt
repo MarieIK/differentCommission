f1d46446a869188762fc04d52fe5bbda7b1f3420
Maria Kuznetsova <mikuznetsova@list.ru>
2022-06-20 00:00:20 +0300

    Code coverage

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,54 +3,62 @@ const val MAESTRO = "Maestro"
 const val VISA = "Visa"
 const val MIR = "Мир"
 const val VK_PAY = "VK Pay"
-const val MIN_COMMISSION_VISA_MIR = 3_500.0
+const val MIN_COMMISSION_VISA_MIR = 3_500
 const val COMMISSION_VISA_MIR = 0.0075
-const val LIMIT_DAY_CARD = 15_000_000.0
-const val LIMIT_MONTH_CARD = 60_000_000.0
-const val LIMIT_DAY_VK_PAY = 1_500_000.0
-const val LIMIT_MONTH_VK_PAY = 4_000_000.0
-const val LIMIT_MASTERCARD_MAESTRO_MONTH = 7_500_000.0
+const val LIMIT_DAY_CARD = 15_000_000
+const val LIMIT_MONTH_CARD = 60_000_000
+const val LIMIT_DAY_VK_PAY = 1_500_000
+const val LIMIT_MONTH_VK_PAY = 4_000_000
+const val LIMIT_MASTERCARD_MAESTRO_MONTH = 7_500_000
 
 fun main() {
-    calculateTotalCommission(MIR, 10_500_000.0, 7_000_000.0)
-    calculateTotalCommission(MASTERCARD, 50_000.0, 15_000_000.0)
-    calculateTotalCommission(VK_PAY, 15_000_000.0, 45_000.0)
+    println(calculateTotalCommission(MASTERCARD, 50_000, 15_000_000))
+    println(calculateTotalCommission(MAESTRO, 60_500_000, 5_000_000))
+    println(calculateTotalCommission(VISA, 8_500_000, 100_000_000))
+    println(calculateTotalCommission(MIR, 10_500_000, 50_000_000))
+    println(calculateTotalCommission(VK_PAY, 15_000_000, 45_000))
+    println(calculateTotalCommission(VK_PAY, 500_000, 10_000))
 }
 
-fun calculateTotalCommission(typeCard: String = "VK Pay", amountTransfer: Double, transactionCurrentMonth: Double = 0.0) {
-    when (typeCard) {
+fun calculateTotalCommission(typeCard: String, amountTransfer: Int, transactionCurrentMonth: Int): String {
+    val total = when (typeCard) {
         MASTERCARD -> calculateCommissionMastercardAndMaestro(amountTransfer, transactionCurrentMonth)
         MAESTRO -> calculateCommissionMastercardAndMaestro(amountTransfer, transactionCurrentMonth)
         VISA -> calculateCommissionVisaAndMir(amountTransfer, transactionCurrentMonth)
         MIR -> calculateCommissionVisaAndMir(amountTransfer, transactionCurrentMonth)
         VK_PAY -> calculateCommissionVkPay(amountTransfer, transactionCurrentMonth)
+        else -> {"Неверные данные"}
     }
+    return total
 }
 
-fun calculateCommissionMastercardAndMaestro(amountTransfer: Double, transactionCurrentMonth: Double) {
-    val commission = amountTransfer * 0.006 + 2_000
-    when {
-        (transactionCurrentMonth >= LIMIT_MONTH_CARD) -> println("Перевод невозможен, превышен месячный лимит: 600 000 руб.")
-        (amountTransfer >= LIMIT_DAY_CARD) -> println("Перевод невозможен, превышен суточный лимит: 150 000 руб.")
-        (transactionCurrentMonth < LIMIT_MASTERCARD_MAESTRO_MONTH) -> println("Комиссия составит 0 коп.")
-        else -> println("Комиссия составит: $commission коп.")
+fun calculateCommissionMastercardAndMaestro(amountTransfer: Int, transactionCurrentMonth: Int): String {
+    val commission = (amountTransfer * 0.006 + 2_000).toInt()
+    val total = when {
+        (transactionCurrentMonth >= LIMIT_MONTH_CARD) -> "Перевод невозможен, превышен месячный лимит: 600 000 руб."
+        (amountTransfer >= LIMIT_DAY_CARD) -> "Перевод невозможен, превышен суточный лимит: 150 000 руб."
+        (transactionCurrentMonth < LIMIT_MASTERCARD_MAESTRO_MONTH) -> "Комиссия составит 0 коп."
+        else -> "Комиссия составит: $commission коп."
     }
+    return total
 }
 
-fun calculateCommissionVisaAndMir(amountTransfer: Double, transactionCurrentMonth: Double) {
-    val commission = amountTransfer * COMMISSION_VISA_MIR
-    when {
-        (transactionCurrentMonth >= LIMIT_MONTH_CARD) -> println("Перевод невозможен, превышен месячный лимит: 600 000 руб.")
-        (amountTransfer >= LIMIT_DAY_CARD) -> println("Перевод невозможен, превышен суточный лимит: 150 000 руб.")
-        (commission <= MIN_COMMISSION_VISA_MIR) -> println("Комиссия составит $MIN_COMMISSION_VISA_MIR коп.")
-        else -> println("Комиссия составит $commission коп.")
+fun calculateCommissionVisaAndMir(amountTransfer: Int, transactionCurrentMonth: Int): String {
+    val commission = (amountTransfer * COMMISSION_VISA_MIR).toInt()
+    val total = when {
+        (transactionCurrentMonth >= LIMIT_MONTH_CARD) -> "Перевод невозможен, превышен месячный лимит: 600 000 руб."
+        (amountTransfer >= LIMIT_DAY_CARD) -> "Перевод невозможен, превышен суточный лимит: 150 000 руб."
+        (commission <= MIN_COMMISSION_VISA_MIR) -> "Комиссия составит $MIN_COMMISSION_VISA_MIR коп."
+        else -> "Комиссия составит $commission коп."
     }
+    return total
 }
 
-fun calculateCommissionVkPay(amountTransfer: Double, transactionCurrentMonth: Double) {
-    when {
-        (transactionCurrentMonth >= LIMIT_MONTH_VK_PAY) -> println("Перевод невозможен, превышен месячный лимит: 40 000 руб.")
-        (amountTransfer >= LIMIT_DAY_VK_PAY) -> println("Перевод невозможен, превышен суточный лимит: 15 000 руб.")
-        else -> println("Комиссия составит 0 коп.")
+fun calculateCommissionVkPay(amountTransfer: Int, transactionCurrentMonth: Int): String {
+    val total = when {
+        (transactionCurrentMonth >= LIMIT_MONTH_VK_PAY) -> "Перевод невозможен, превышен месячный лимит: 40 000 руб."
+        (amountTransfer >= LIMIT_DAY_VK_PAY) -> "Перевод невозможен, превышен суточный лимит: 15 000 руб."
+        else -> "Комиссия составит 0 коп."
     }
+    return total
 }
---
+++ src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,112 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MainKtTest {
+
+    @Test
+    fun commissionMasterCard() {
+        val typeCard = "Mastercard"
+        val amountTransfer = 50_000
+        val transactionCurrentMonth = 15_000_000
+
+        val result = calculateTotalCommission(typeCard, amountTransfer, transactionCurrentMonth)
+
+        assertEquals("Комиссия составит: 2300 коп.", result)
+    }
+
+    @Test
+    fun commissionMaestro() {
+        val typeCard = "Maestro"
+        val amountTransfer = 11_500_000
+        val transactionCurrentMonth = 5_000_000
+
+        val result = calculateTotalCommission(typeCard, amountTransfer, transactionCurrentMonth)
+
+        assertEquals("Комиссия составит 0 коп.", result)
+    }
+
+    @Test
+    fun commissionCardIfExceedLimitDay() {
+        val maestro = "Maestro"
+        val visa = "Visa"
+        val amountTransfer = 60_500_000
+        val transactionCurrentMonth = 5_000_000
+
+        val resultMaestro = calculateTotalCommission(maestro, amountTransfer, transactionCurrentMonth)
+        val resultVisa = calculateTotalCommission(visa, amountTransfer, transactionCurrentMonth)
+
+        assertEquals("Перевод невозможен, превышен суточный лимит: 150 000 руб.", resultMaestro)
+        assertEquals("Перевод невозможен, превышен суточный лимит: 150 000 руб.", resultVisa)
+    }
+
+
+    @Test
+    fun commissionCardIfExceedLimitMonth() {
+        val visa = "Visa"
+        val mastercard = "Mastercard"
+        val amountTransfer = 8_500_000
+        val transactionCurrentMonth = 100_000_000
+
+        val resultVisa = calculateTotalCommission(visa, amountTransfer, transactionCurrentMonth)
+        val resultMastercard = calculateTotalCommission(mastercard, amountTransfer, transactionCurrentMonth)
+
+        assertEquals("Перевод невозможен, превышен месячный лимит: 600 000 руб.", resultVisa)
+        assertEquals("Перевод невозможен, превышен месячный лимит: 600 000 руб.", resultMastercard)
+    }
+
+    @Test
+    fun commissionMir() {
+        val typeCard = "Мир"
+        val amountTransfer = 10_500_000
+        val transactionCurrentMonth = 50_000_000
+
+        val result = calculateTotalCommission(typeCard, amountTransfer, transactionCurrentMonth)
+
+        assertEquals("Комиссия составит 78750 коп.", result)
+    }
+
+    @Test
+    fun minCommissionVisaAndMir() {
+        val typeCard = "Мир"
+        val amountTransfer = 3_500
+        val transactionCurrentMonth = 50_000_000
+
+        val result = calculateTotalCommission(typeCard, amountTransfer, transactionCurrentMonth)
+
+        assertEquals("Комиссия составит 3500 коп.", result)
+    }
+
+    @Test
+    fun commissionVkPay() {
+        val typeCard = "VK Pay"
+        val amountTransfer = 1_500_000
+        val transactionCurrentMonth = 4_500_000
+
+        val result = calculateTotalCommission(typeCard, amountTransfer, transactionCurrentMonth)
+
+        assertEquals("Перевод невозможен, превышен месячный лимит: 40 000 руб.", result)
+    }
+
+    @Test
+    fun commissionVkPayEqualsNull() {
+        val typeCard = "VK Pay"
+        val amountTransfer = 500_000
+        val transactionCurrentMonth = 10_000
+
+        val result = calculateTotalCommission(typeCard, amountTransfer, transactionCurrentMonth)
+
+        assertEquals("Комиссия составит 0 коп.", result)
+    }
+
+    @Test
+    fun invalidInput() {
+        val typeCard = " "
+        val amountTransfer = 15_000_000
+        val transactionCurrentMonth = 45_000
+
+        val result = calculateTotalCommission(typeCard, amountTransfer, transactionCurrentMonth)
+
+        assertEquals("Неверные данные", result)
+    }
+}
